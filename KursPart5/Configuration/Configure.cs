9b87ad7b9b129f5af0dc95b45249f7e25deab2c6
alicja.m.musial <alicja.m.musial@gmail.com>
2020-01-09 23:10:29 +0100

    Add project files.

---
+++ KursPart5/Configuration/Configure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+
+namespace KursPart5.Configuration
+{
+     public class Configure : IConfigure
+     {
+        public void BrowserChoose()
+        {
+            Console.WriteLine("Select browser:\n");
+            Console.WriteLine("IE = 1 | Chrome = 2");
+            int option = Convert.ToInt32(Console.ReadLine());
+            if (option == 1 || option == 2)
+            {
+                if (option == 1)
+                {
+                    IWebDriver driver = new InternetExplorerDriver();
+                    driver.Manage().Window.Maximize();
+                }
+                if (option == 2)
+                {
+                    IWebDriver driver = new ChromeDriver();
+                    driver.Manage().Window.Maximize();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error! We will use Chrome!");
+            }
+        }
+    }
+}
---
+++ KursPart5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using KursPart5.Configuration;
+
+namespace KursPart5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Configure.BrowserChoose();
+        }
+    }
+}
